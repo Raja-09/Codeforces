e6f9cb623a00f0f8dc31372a62da84ed565a798f
Raja <aravindha92@gmail.com>
2022-01-08 22:09:07 +0530

    implementation

--- 1359A-BerlandPoker.py
+++ 1359A-BerlandPoker.py
@@ -1,21 +1,26 @@
-t=int(input())
-for _ in range(t):
+
+for _ in range(int(input())):
     n,m,k=map(int,input().split())
     x=n/k
-    print("This is the cards that the first player gets",x)
+    # print(x)
     if n/k<m:
         #in this case the jokers are more
         remjoker=m-(n/k)
-        print("The left oiut jokers:",remjoker)
+        # print(remjoker)
         #now distribute the remainging jokers among the rest(k-1)
         dist=remjoker/(k-1)
-        print("This is the number of cards the rest players get",dist)
+        if(dist!=int(dist)):
+            dist = int(dist) +1 
+        else:
+            dist = int(dist)
+        y = dist
+        # print(dist)
+        
         
     else:
         #in this case all the jokers are taken by one player
         x=m
         y=0
-    print(x-y)
-    print()
+    print(int(x-y))
 
     
